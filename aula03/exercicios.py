e202c07779fd59276435060625430c9f6147fae7
jusluz <julioluz84@yahoo.com>
2024-09-23 12:49:29 -0300

    inicio da realização dos exercícios

--- aula03/exercicios.py
+++ aula03/exercicios.py
@@ -5,6 +5,10 @@
 # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
+quantidade = int(input('Digite o valor da quantidade de vendas: '))
+preco = float(input('Digite o valor do preço: '))
+
+'Dados válidos' if quantidade > 0 and preco > 0 else 'Dados inválidos'
 
 ### Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. 
@@ -13,53 +17,121 @@
 # Temperatura < 18°C é 'Baixa'
 # Temperatura >= 18°C e <= 26°C é 'Normal'
 # Temperatura > 26°C é 'Alta'
+temperatura = int(input('Digite a temperatura em graus celsius: '))
+
+'Baixa' if temperatura < 18 else 'Normal' if temperatura < 27 else 'Alta'
 
 ### Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens 
 # com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+log = {'timestamp': '2021-06-23 10:00:00',
+       'level': 'ERROR', 'message': 'Falha na conexão'}
+
+if log['level'] == 'ERROR':
+    print(log['message'])
 
 ### Exercício 4: Validação de Dados de Entrada
 # Antes de processar os dados de usuários em um sistema de recomendação, 
 # você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
 # fornecido um email válido. Escreva um programa que valide essas condições 
 # e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+idade = int(input('Digite sua idade: '))
+email = input('Digite seu e-mail: ')
+
+if idade < 18 or idade > 65:
+    print('Idade inválida. A idade deve estar entre 18 e 65 anos')
+    if '@' not in email:
+        print('E-mail inválido. Por favor, forneça um e-mail válido')
+else:
+    print('Dados de usuário válidos')
 
 ### Exercício 5: Detecção de Anomalias em Dados de Transações
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+tranferencia = float(input('Digite o valor da transferência: R$ '))
+horario_da_transacao = input('Digite o forario da transação ex. 17:30')
+repl_horario = horario_da_transacao.replace(':', '.')
+horario_da_transacao = round(float(repl_horario), 2)
+
+if tranferencia > 10000:
+    if horario_da_transacao < 9 or horario_da_transacao > 18:
+        print('Transação suspeita')
+else:
+    print('Transação normal\n')
+    print('O valor será adicionado a sua conta.')
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
+# Entrada: Um texto qualquer.
+# Saída: Uma lista de tuplos, onde cada tuplo contém uma palavra e sua
+# contagem.
+texto = input('Digite um texto: ')
+palavras = texto.split()
+contagem_palavras = {}
+
+for palavra in palavras:
+    palavra = palavra.lower()
+    if palavra in contagem_palavras:
+        contagem_palavras[palavra] += 1
+
+    else:
+        contagem_palavras[palavra] = 1
+contagem_palavras_desc = dict(sorted(contagem_palavras.items(), key=lambda item: item[1], reverse=True))
+print(contagem_palavras_desc)
 
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
 
+
+
+
 ### Exercício 8. Filtragem de Dados Faltantes
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
+
+
+
 
 ### Exercício 9. Extração de Subconjuntos de Dados
 # Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
 
+
+
+
 ### Exercício 10. Agregação de Dados por Categoria
 # Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+
+
+
 
 ### Exercícios com WHILE
 
 ### Exercício 11. Leitura de Dados até Flag
 # Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
 
+
+
+
 ### Exercício 12. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+
+
+
 
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
 
+
+
+
 ### Exercício 14. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+
+
+
 
 ### Exercício 15. Processamento de Dados com Condição de Parada
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
